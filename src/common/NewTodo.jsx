f97ce40ab73294eb7b367c19db435c3f408b5bb7
Erfan Shafiee <erfan74sh@gmail.com>
2022-02-21 22:55:51 +0330

    create submitHandler and style animations and states

--- src/common/NewTodo.jsx
+++ src/common/NewTodo.jsx
@@ -4,6 +4,10 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 const NewTodo = () => {
+	const handleSubmit = (e) => {
+		e.preventDefault();
+	};
+
 	return (
 		<div className="flex flex-col gap-y-2">
 			<input
@@ -11,8 +15,15 @@ const NewTodo = () => {
 				placeholder="Enter Todo..."
 				className="h-9 rounded-md px-2 outline-none focus:ring-2"
 			/>
-			<button className="bg-blue-100 rounded-md flex items-center justify-center w-full h-9">
-				<FontAwesomeIcon icon={faPlus} className="text-sky-900" />
+			<button
+				type="submit"
+				className="bg-blue-100 rounded-md flex items-center justify-center w-full h-9 hover:bg-blue-200 group transition"
+				onClick={(e) => handleSubmit(e)}
+			>
+				<FontAwesomeIcon
+					icon={faPlus}
+					className="text-sky-900 transform group-active:scale-125 transition"
+				/>
 			</button>
 		</div>
 	);
