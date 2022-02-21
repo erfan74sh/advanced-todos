5587b68bd82d6692be4d60b20c4c4eebc2eec95e
Erfan Shafiee <erfan74sh@gmail.com>
2022-02-21 23:01:36 +0330

    change style of counter todos

--- src/common/Group.jsx
+++ src/common/Group.jsx
@@ -8,7 +8,9 @@ const Group = () => {
 		<article className="w-72 bg-blue-50 rounded-xl p-4 flex flex-col gap-y-3">
 			<header className="flex justify-between items-center">
 				<h2 className="font-medium capitalize">group name</h2>
-				<span className="py-0.5 px-2 max-w-full rounded bg-blue-100">2</span>
+				<span className="py-0.5 px-2 max-w-full rounded bg-blue-100 text-sm text-sky-900">
+					2
+				</span>
 			</header>
 			<NewTodo />
 			<ul className="flex flex-col gap-y-2.5">
