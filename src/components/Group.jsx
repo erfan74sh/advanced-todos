b7fcb9d81e918833e076c3c5dab7d1f204248072
Erfan Shafiee <erfan74sh@gmail.com>
2022-02-21 21:21:25 +0330

    create Group component

---
+++ src/components/Group.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Group = () => {
+	return (
+		<article>
+			<header>
+				<h2>group name</h2>
+				<span>2</span>
+			</header>
+			<ul>
+				<li>todo card here</li>
+			</ul>
+		</article>
+	);
+};
+
+export default Group;
