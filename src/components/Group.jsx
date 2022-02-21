2414a2a0a248e3d98033c7eeaf6b57669767370b
Erfan Shafiee <erfan74sh@gmail.com>
2022-02-21 21:38:03 +0330

    create Group layout and header and style the component

--- src/components/Group.jsx
+++ src/components/Group.jsx
@@ -2,12 +2,15 @@ import React from "react";
 
 const Group = () => {
 	return (
-		<article>
-			<header>
-				<h2>group name</h2>
-				<span>2</span>
+		<article className="w-72 bg-blue-50 rounded-xl p-4 flex flex-col gap-y-3">
+			<header className="flex justify-between items-center">
+				<h2 className="font-medium capitalize">group name</h2>
+				<span className="py-0.5 px-2 max-w-full rounded bg-blue-100">2</span>
 			</header>
-			<ul>
+			<div>
+				<input type="text" />
+			</div>
+			<ul className="flex flex-col gap-y-2">
 				<li>todo card here</li>
 			</ul>
 		</article>
