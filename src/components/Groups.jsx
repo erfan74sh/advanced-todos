c9a75f20f7bd2fed8e006c970f3a4dd7b3597d40
Erfan Shafiee <erfan74sh@gmail.com>
2022-02-21 23:00:02 +0330

    create Groups component for rendering Group components

---
+++ src/components/Groups.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+// components
+import Group from "../common/Group";
+
+const Groups = () => {
+	return (
+		<div className="flex gap-x-10">
+			<Group />
+			<Group />
+			<Group />
+		</div>
+	);
+};
+
+export default Groups;
