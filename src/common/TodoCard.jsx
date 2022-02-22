b72552e8514a3f9e172f692d073c13f11a172eb5
Erfan Shafiee <erfan74sh@gmail.com>
2022-02-22 18:37:25 +0330

    create showDropdown state

--- src/common/TodoCard.jsx
+++ src/common/TodoCard.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import MoreOptions from "./MoreOptions";
 // components
 
 const TodoCard = () => {
+	const [showDropdown, setShowDropdown] = useState(false);
+
 	return (
 		<li className="bg-white rounded-md py-2 px-2.5">
 			<article>
@@ -10,7 +12,10 @@ const TodoCard = () => {
 					<h3 className="font-medium flex justify-between gap-x-0.5 relative">
 						<span className="pr-6">title of todo 111111111111111111 11</span>
 						<div className="absolute right-0">
-							<MoreOptions />
+							<MoreOptions
+								showDropdown={showDropdown}
+								handleDropdown={() => setShowDropdown(!showDropdown)}
+							/>
 						</div>
 					</h3>
 				</header>
