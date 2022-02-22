6f4002ce47a70656a7fcb6d7c57f2c5e46b6da68
Erfan Shafiee <erfan74sh@gmail.com>
2022-02-22 18:11:21 +0330

    add more options to option list

--- src/common/MoreOptions.jsx
+++ src/common/MoreOptions.jsx
@@ -15,18 +15,39 @@ const MoreOptions = () => {
 			<span className="bg-stone-50 w-6 h-6 flex items-center justify-center cursor-pointer hover:bg-stone-200 rounded transition">
 				<FontAwesomeIcon icon={faEllipsisVertical} className="text-sky-900" />
 			</span>
-			<ul className="bg-red-500 absolute z-10 top-full transform translate-y-1 p-2.5 rounded">
-				<li className="flex gap-x-1 items-center">
-					<span className="w-4 h-4 bg-green-600 flex items-center justify-center">
-						<FontAwesomeIcon icon={faTrashCan} className="text-sm" />
+			<ul className="bg-stone-50 absolute z-10 top-full transform translate-y-1 p-2.5 rounded shadow-md">
+				<li className="flex gap-x-2 items-center">
+					<span className="w-4 h-4  flex items-center justify-center">
+						<FontAwesomeIcon
+							icon={faPenToSquare}
+							className="text-sm text-zinc-400"
+						/>
 					</span>
-					<span>option</span>
+					<span>rename</span>
 				</li>
-				<li className="flex gap-x-1 items-center">
-					<span className="w-4 h-4 bg-green-600 flex items-center justify-center">
-						<FontAwesomeIcon icon={faTrashCan} className="text-sm" />
+				<li className="flex gap-x-2 items-center">
+					<span className="w-4 h-4  flex items-center justify-center">
+						<FontAwesomeIcon
+							icon={faTrashCan}
+							className="text-sm text-zinc-400"
+						/>
 					</span>
-					<span>option</span>
+					<span>delete</span>
+				</li>
+				<li className="flex gap-x-2 items-center">
+					<span className="w-4 h-4  flex items-center justify-center">
+						<FontAwesomeIcon icon={faClone} className="text-sm text-zinc-400" />
+					</span>
+					<span>duplicate</span>
+				</li>
+				<li className="flex gap-x-2 items-center">
+					<span className="w-4 h-4  flex items-center justify-center">
+						<FontAwesomeIcon
+							icon={faRightFromBracket}
+							className="text-sm text-zinc-400"
+						/>
+					</span>
+					<span>move to</span>
 				</li>
 			</ul>
 		</>
