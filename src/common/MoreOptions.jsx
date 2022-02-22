41aec78e5b93a44b69289f162548385c572df078
Erfan Shafiee <erfan74sh@gmail.com>
2022-02-22 18:34:23 +0330

    handleShowing dropdown on click

--- src/common/MoreOptions.jsx
+++ src/common/MoreOptions.jsx
@@ -9,14 +9,24 @@ import {
 	faPenToSquare,
 } from "@fortawesome/free-solid-svg-icons";
 
-const MoreOptions = () => {
+const MoreOptions = ({ handleDropdown, showDropdown }) => {
 	return (
 		<>
-			<span className="bg-stone-50 w-6 h-6 flex items-center justify-center cursor-pointer hover:bg-stone-200 rounded transition">
+			<span
+				onClick={() => handleDropdown()}
+				className="bg-stone-50 w-6 h-6 flex items-center justify-center cursor-pointer hover:bg-stone-200 rounded transition"
+			>
 				<FontAwesomeIcon icon={faEllipsisVertical} className="text-sky-900" />
 			</span>
-			<ul className="bg-white absolute z-10 top-full transform translate-y-1 p-2.5 rounded shadow-md w-40">
-				<li className="flex gap-x-2 items-center px-1 py-0.5 hover:bg-stone-200 cursor-pointer rounded font-normal transition-colors duration-75">
+			<ul
+				className={`${
+					showDropdown ? "block" : "hidden"
+				} bg-white absolute z-10 top-full transform translate-y-1 p-2.5 rounded shadow-md w-40`}
+			>
+				<li
+					className="flex gap-x-2 items-center px-1 py-0.5 hover:bg-stone-200 cursor-pointer rounded font-normal transition-colors duration-75"
+					onClick={() => handleDropdown()}
+				>
 					<span className="w-4 h-4  flex items-center justify-center">
 						<FontAwesomeIcon
 							icon={faPenToSquare}
@@ -25,7 +35,10 @@ const MoreOptions = () => {
 					</span>
 					<span>rename</span>
 				</li>
-				<li className="flex gap-x-2 items-center px-1 py-0.5 hover:bg-stone-200 cursor-pointer rounded font-normal transition-colors duration-75">
+				<li
+					className="flex gap-x-2 items-center px-1 py-0.5 hover:bg-stone-200 cursor-pointer rounded font-normal transition-colors duration-75"
+					onClick={() => handleDropdown()}
+				>
 					<span className="w-4 h-4  flex items-center justify-center">
 						<FontAwesomeIcon
 							icon={faTrashCan}
@@ -34,7 +47,10 @@ const MoreOptions = () => {
 					</span>
 					<span>delete</span>
 				</li>
-				<li className="flex gap-x-2 items-center px-1 py-0.5 hover:bg-stone-200 cursor-pointer rounded font-normal transition-colors duration-75">
+				<li
+					className="flex gap-x-2 items-center px-1 py-0.5 hover:bg-stone-200 cursor-pointer rounded font-normal transition-colors duration-75"
+					onClick={() => handleDropdown()}
+				>
 					<span className="w-4 h-4  flex items-center justify-center">
 						<FontAwesomeIcon
 							icon={faClone}
@@ -43,7 +59,10 @@ const MoreOptions = () => {
 					</span>
 					<span>duplicate</span>
 				</li>
-				<li className="flex gap-x-2 items-center px-1 py-0.5 hover:bg-stone-200 cursor-pointer rounded font-normal transition-colors duration-75">
+				<li
+					className="flex gap-x-2 items-center px-1 py-0.5 hover:bg-stone-200 cursor-pointer rounded font-normal transition-colors duration-75"
+					onClick={() => handleDropdown()}
+				>
 					<span className="w-4 h-4  flex items-center justify-center">
 						<FontAwesomeIcon
 							icon={faRightFromBracket}
