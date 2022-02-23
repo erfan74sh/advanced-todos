d325622a5869626731ca6b4e2fda129e814877aa
Erfan Shafiee <erfan74sh@gmail.com>
2022-02-23 20:48:43 +0330

    dynamic groupName

--- src/common/Group.jsx
+++ src/common/Group.jsx
@@ -3,11 +3,11 @@ import React from "react";
 import TodoCard from "./TodoCard";
 import NewTodo from "./NewTodo";
 
-const Group = () => {
+const Group = ({ groupName }) => {
 	return (
 		<article className="w-72 bg-blue-50 rounded-xl p-4 flex flex-col gap-y-3">
 			<header className="flex justify-between items-center">
-				<h2 className="font-medium capitalize">group name</h2>
+				<h2 className="font-medium capitalize">{groupName}</h2>
 				<span className="py-0.5 px-2 max-w-full rounded bg-blue-100 text-sm text-sky-900">
 					2
 				</span>
