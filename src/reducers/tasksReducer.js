6bdf8bc22ff245067d7ea112e34d685f91cfbb5a
Erfan Shafiee <erfan74sh@gmail.com>
2022-02-23 20:28:00 +0330

    create tasksReducer and initial state

---
+++ src/reducers/tasksReducer.js
@@ -0,0 +1,23 @@
+export const initialState = [
+	{
+		title: "sample task title...",
+		description: "sample task description...",
+		group: "todo",
+	},
+];
+
+const tasksReducer = (state, action) => {
+	const { type, payload } = action;
+	switch (type) {
+		case "ADD_TASK_TO_GROUP":
+			console.log("ADD_TASK_TO_GROUP", payload);
+			return state;
+		case "REMOVE_TASK_FROM_GROUP":
+			console.log("REMOVE_TASK_FROM_GROUP", payload);
+			return state;
+		default:
+			return state;
+	}
+};
+
+export default tasksReducer;
