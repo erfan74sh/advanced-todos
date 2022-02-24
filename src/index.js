5612801cd64497f03e6151c3d8946c3977f61642
Erfan Shafiee <erfan74sh@gmail.com>
2022-02-24 15:45:41 +0330

    add routes to index.js file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+// routes
 import App from "./App";
+import Modal from "./common/Modal";
+// style
+import "./index.css";
 
 ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
+	<BrowserRouter>
+		<Routes>
+			<Route path="/" element={<App />}>
+				<Route path=":taskId" element={<Modal />} />
+			</Route>
+		</Routes>
+	</BrowserRouter>,
 	document.getElementById("root")
 );
