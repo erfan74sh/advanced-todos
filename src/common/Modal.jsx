ebfd7e5203a25a27715644809a3b61c744d5fb06
Erfan Shafiee <erfan74sh@gmail.com>
2022-02-24 16:13:25 +0330

    create Modal component layout

--- src/common/Modal.jsx
+++ src/common/Modal.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Modal = () => {
-	return <div className=" bg-red-300 z-50">Modal</div>;
+	return (
+		<div className="fixed w-screen h-screen bg-white z-50 bg-opacity-5 backdrop-filter backdrop-blur-sm">
+			<div className="w-full h-full sm:px-10 flex items-center justify-center">
+				<div className="w-full md:w-4/5 lg:w-3/5 bg-red-400 rounded-lg px-5 py-2 ">
+					Modal
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Modal;
