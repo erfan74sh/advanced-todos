a9735c35e40c14d36c3f937e12e997f89c32adf9
Erfan Shafiee <erfan74sh@gmail.com>
2022-02-25 12:46:53 +0330

    add another radio input for canceling sort or filter

--- src/components/sortAndFilter/SortAndFilter.js
+++ src/components/sortAndFilter/SortAndFilter.js
@@ -1,35 +1,68 @@
-import { faFilter, faSort } from "@fortawesome/free-solid-svg-icons";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+// components
 import FilterOptions from "./FilterOptions";
 import SortOptions from "./SortOptions";
+// icons
+import { faFilter, faSort, faXmark } from "@fortawesome/free-solid-svg-icons";
 
 const SortAndFilter = () => {
+	const [sortOrFilter, setSortOrFilter] = useState("");
+
+	const handleChange = (e) => {
+		setSortOrFilter(e.target.value);
+	};
+
 	return (
 		<div className="flex flex-col text-zinc-700">
-			<form className="flex gap-x-3 justify-end">
-				<label className="flex items-center gap-x-1 px-3 py-1.5 leading-none cursor-pointer">
+			<form
+				className="flex gap-x-3 justify-end"
+				onChange={(e) => handleChange(e)}
+			>
+				{sortOrFilter !== "" && (
+					<label className="flex items-center p-1.5 rounded leading-none cursor-pointer bg-zinc-100 hover:text-sky-600 transition-colors">
+						<span className="w-4 h-4 flex items-center justify-center">
+							<FontAwesomeIcon icon={faXmark} className="" />
+						</span>
+
+						<input
+							type="radio"
+							name="sort-and-filter"
+							value=""
+							className="hidden"
+						/>
+					</label>
+				)}
+				<label
+					className={`flex items-center gap-x-1 px-3 py-1.5 rounded leading-none cursor-pointer ${
+						sortOrFilter === "filter" && "text-sky-600 bg-zinc-100"
+					}`}
+				>
 					<span className="w-4 h-4 flex items-center justify-center">
-						<FontAwesomeIcon icon={faSort} className="text-zinc-500 text-lg" />
+						<FontAwesomeIcon icon={faSort} className=" text-lg" />
 					</span>
 					<span>Filter</span>
 					<input
 						type="radio"
 						name="sort-and-filter"
 						value="filter"
-						className=""
+						className="hidden"
 					/>
 				</label>
-				<label className="flex items-center gap-x-1 px-3 py-1.5 leading-none cursor-pointer">
+				<label
+					className={`flex items-center gap-x-1 px-3 py-1.5 rounded leading-none cursor-pointer ${
+						sortOrFilter === "sort" && "text-sky-600 bg-zinc-100"
+					}`}
+				>
 					<span className="w-4 h-4 flex items-center justify-center">
-						<FontAwesomeIcon icon={faFilter} className="text-zinc-500" />
+						<FontAwesomeIcon icon={faFilter} className="" />
 					</span>
 					<span>Sort</span>
 					<input
 						type="radio"
 						name="sort-and-filter"
 						value="sort"
-						className=""
+						className="hidden"
 					/>
 				</label>
 			</form>
