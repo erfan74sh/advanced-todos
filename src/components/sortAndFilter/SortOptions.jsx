52e7e5062b5ae8bcde559a4aa6c131f92ed9133d
Erfan Shafiee <erfan74sh@gmail.com>
2022-02-25 13:55:52 +0330

    create SortOptions content and overall layout

--- src/components/sortAndFilter/SortOptions.jsx
+++ src/components/sortAndFilter/SortOptions.jsx
@@ -1,7 +1,49 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// icons
+import {
+	faArrowDownShortWide,
+	faArrowDownWideShort,
+} from "@fortawesome/free-solid-svg-icons";
 
 const SortOptions = () => {
-	return <div>SortOptions</div>;
+	return (
+		<form className="flex gap-x-3 divide-x-2">
+			<fieldset>
+				<div>order</div>
+				<ul>
+					<li>
+						<label className="flex items-center gap-x-2">
+							<FontAwesomeIcon icon={faArrowDownShortWide} />
+							<span>ascending</span>
+							<input type="radio" name="sort-order" value="ascending" />
+						</label>
+					</li>
+					<li>
+						<label className="flex items-center gap-x-2">
+							<FontAwesomeIcon icon={faArrowDownWideShort} />
+							<span>descending</span>
+							<input type="radio" name="sort-order" value="descending" />
+						</label>
+					</li>
+				</ul>
+			</fieldset>
+			<fieldset className="pl-3">
+				<label>
+					<span>Name</span>
+					<input type="radio" name="sortBy" value="name" />
+				</label>
+				<label>
+					<span>Date</span>
+					<input type="radio" name="sortBy" value="date" />
+				</label>
+				<label>
+					<span>Priority</span>
+					<input type="radio" name="sortBy" value="priority" />
+				</label>
+			</fieldset>
+		</form>
+	);
 };
 
 export default SortOptions;
