db0bb642be63d8b15ea71316ada5926dbf7e8eb2
Erfan Shafiee <erfan74sh@gmail.com>
2022-02-25 19:00:44 +0330

    create Nav section in modal

--- src/common/Modal.jsx
+++ src/common/Modal.jsx
@@ -1,6 +1,9 @@
+import {  faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useEffect, useRef, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import useOnClickOutside from "../hooks/useOnClickOutside";
+import MoreOptions from "./MoreOptions";
 import { useTasksContext } from "./providers/TasksProvider";
 
 const Modal = () => {
@@ -13,6 +16,7 @@ const Modal = () => {
 	useEffect(() => {
 		const thisTask = tasks.filter((task) => task.id === parseInt(taskId))?.[0];
 		thisTask && setTaskToEdit(thisTask);
+		console.log(thisTask)
 	}, [tasks, taskId]);
 
 	const onClickOutside = () => {
@@ -25,10 +29,21 @@ const Modal = () => {
 		<div className="fixed w-screen h-screen bg-white z-50 bg-opacity-5 backdrop-filter backdrop-blur-sm">
 			<div className="w-full h-full sm:px-10 flex items-center justify-center">
 				<div
-					className="w-full md:w-4/5 lg:w-3/5 bg-red-400 rounded-lg px-5 py-2 "
+					className="w-full md:w-4/5 lg:w-3/5 h-2/3 overflow-y-scroll bg-red-400 rounded-lg px-5 py-2 "
 					ref={modalBgRef}
 				>
-					<article className="flex flex-col gap-y-4">
+				<nav className="flex justify-between items-center">
+					<div className="flex items-center gap-x-1">
+						<span className="w-6 h-6 flex items-center justify-center hover:bg-zinc-100 rounded transition-colors">
+							<FontAwesomeIcon icon={faChevronUp}/>
+						</span>
+						<span className="w-6 h-6 flex items-center justify-center hover:bg-zinc-100 rounded transition-colors">
+							<FontAwesomeIcon icon={faChevronDown}/>
+						</span>
+					</div>
+					<MoreOptions/>
+				</nav>
+					<article className="flex flex-col gap-y-4 mt-10 px-10 py-5">
 						<header>
 							<input
 								type="text"
@@ -41,7 +56,7 @@ const Modal = () => {
 							<ul className="flex flex-col gap-y-3">
 								<li className="flex gap-x-4">
 									<span className="w-1/2 sm:w-1/3 lg:w-1/4">created date:</span>
-									<span className="w-1/2 sm:w-1/3 lg:w-1/4">date</span>
+									<span className="w-1/2 sm:w-1/3 lg:w-1/4">{taskToEdit.createdDate}</span>
 								</li>
 								<li className="flex gap-x-4">
 									<span className="w-1/2 sm:w-1/3 lg:w-1/4">status:</span>
@@ -55,12 +70,11 @@ const Modal = () => {
 								</li>
 							</ul>
 						</section>
-						<section>
-							<h4>description:</h4>
+						<section className="flex-grow">
 							<textarea
 								value={taskToEdit.description}
 								placeholder="more description..."
-								className="outline-none"
+								className="outline-none w-full"
 							/>
 						</section>
 					</article>
