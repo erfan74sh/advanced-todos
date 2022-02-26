cda5b188cb46072b55998ff91f6f1cf9ee8a6832
Erfan Shafiee <erfan74sh@gmail.com>
2022-02-26 20:44:44 +0330

    edit moveToGeoup Component and make it a dropdown list

--- src/common/MoveToGroup.jsx
+++ src/common/MoveToGroup.jsx
@@ -1,42 +1,58 @@
-import React from "react";
+import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
 
 const MoveToGroup = () => {
+	const [showDropdown, setShowDropdown] = useState(false);
+	const handleShowDropdown = () => {
+		setShowDropdown(!showDropdown);
+	};
+
 	return (
-		<ul className="absolute left-full transform translate-x-4 bg-white rounded shadow-md p-2.5 flex flex-col top-0">
-			<li className="">
-				<label className="block px-1 py-0.5 hover:bg-stone-200 cursor-pointer rounded font-normal transition-colors duration-75 ">
-					<input
-						type="radio"
-						name="groupName"
-						value="todo"
-						className="hidden"
-					/>
-					<span>todo</span>
-				</label>
-			</li>
-			<li className="">
-				<label className="block px-1 py-0.5 hover:bg-stone-200 cursor-pointer rounded font-normal transition-colors duration-75 ">
-					<input
-						type="radio"
-						name="groupName"
-						value="doing"
-						className="hidden"
-					/>
-					<span>doing</span>
-				</label>
-			</li>
-			<li className="">
-				<label className="block px-1 py-0.5 hover:bg-stone-200 cursor-pointer rounded font-normal transition-colors duration-75 ">
-					<input
-						type="radio"
-						name="groupName"
-						value="completed"
-						className="hidden"
-					/>
-					<span>completed</span>
-				</label>
-			</li>
-		</ul>
+		<>
+			<div
+				className={`flex justify-between rounded-t px-2 py-1 ${
+					showDropdown && "shadow-md"
+				}`}
+				onClick={() => handleShowDropdown()}
+			>
+				<span>status</span>
+				<span>
+					<FontAwesomeIcon icon={faCaretDown} />
+				</span>
+			</div>
+			{showDropdown && (
+				<form className="absolute left-0 w-full bg-white rounded-b shadow-md p-2.5 flex flex-col top-full border-t-2">
+					<label className="block px-1 py-0.5 hover:bg-stone-200 cursor-pointer rounded font-normal transition-colors duration-75 ">
+						<input
+							type="radio"
+							name="groupName"
+							value="todo"
+							className="hidden"
+						/>
+						<span>todo</span>
+					</label>
+					<label className="block px-1 py-0.5 hover:bg-stone-200 cursor-pointer rounded font-normal transition-colors duration-75 ">
+						<input
+							type="radio"
+							name="groupName"
+							value="doing"
+							className="hidden"
+						/>
+						<span>doing</span>
+					</label>
+					<label className="block px-1 py-0.5 hover:bg-stone-200 cursor-pointer rounded font-normal transition-colors duration-75 ">
+						<input
+							type="radio"
+							name="groupName"
+							value="completed"
+							className="hidden"
+						/>
+						<span>completed</span>
+					</label>
+				</form>
+			)}
+		</>
 	);
 };
 
