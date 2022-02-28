0b49a548ae55094c027189fd64f36c896795d2fe
Erfan Shafiee <erfan74sh@gmail.com>
2022-02-28 20:06:07 +0330

    create AddTags component

---
+++ src/common/AddTags.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// iconst
+import { faCaretDown, faPlus } from "@fortawesome/free-solid-svg-icons";
+
+const AddTags = () => {
+	const [showDropdown, setShowDropdown] = useState(false);
+	const handleShowDropdown = () => {
+		setShowDropdown(!showDropdown);
+	};
+	return (
+		<>
+			<div
+				className={`flex justify-between rounded px-2 py-1 hover:shadow cursor-pointer text-zinc-600 transition-all ${
+					showDropdown && "shadow-md rounded-b-none"
+				}`}
+				onClick={() => handleShowDropdown()}
+			>
+				<span>Tags</span>
+				<span>
+					<FontAwesomeIcon icon={faCaretDown} />
+				</span>
+			</div>
+			{showDropdown && (
+				<form
+					className="absolute left-0 w-full bg-white rounded-b shadow-md p-2.5 flex flex-col top-full border-t-2 z-20"
+					// onInput={(e) => handleOnChange(e)}
+				>
+					<label className="flex px-1 py-0.5 hover:bg-stone-200 cursor-pointer rounded font-normal transition-colors duration-75 focus-within:ring-2 mb-1">
+						<input
+							type="text"
+							className="w-full bg-transparent outline-none text-sm"
+							placeholder="add new tag..."
+						/>
+						<span className="hover:bg-slate-50 rounded px-1">
+							<FontAwesomeIcon icon={faPlus} className="text-zinc-700" />
+						</span>
+					</label>
+					<label className="block px-1 py-0.5 hover:bg-stone-200 cursor-pointer rounded font-normal transition-colors duration-75 ">
+						<input type="radio" name="group" value="todo" className="hidden" />
+						<span>todo</span>
+					</label>
+					<label className="block px-1 py-0.5 hover:bg-stone-200 cursor-pointer rounded font-normal transition-colors duration-75 ">
+						<input type="radio" name="group" value="doing" className="hidden" />
+						<span>doing</span>
+					</label>
+					<label className="block px-1 py-0.5 hover:bg-stone-200 cursor-pointer rounded font-normal transition-colors duration-75 ">
+						<input
+							type="radio"
+							name="group"
+							value="completed"
+							className="hidden"
+						/>
+						<span>completed</span>
+					</label>
+				</form>
+			)}
+		</>
+	);
+};
+
+export default AddTags;
