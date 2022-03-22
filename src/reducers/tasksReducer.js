a63e5155641f5ecf6df77144e03056ab9e992741
erfan shafiee <erfan74sh@gmail.com>
2022-03-22 16:36:57 +0430

    add tags array to initial state

--- src/reducers/tasksReducer.js
+++ src/reducers/tasksReducer.js
@@ -1,34 +1,34 @@
-export const initialState = [
-	{
-		title: "sample task title...",
-		description: "sample task description...",
-		group: "todo",
-		id: 1,
-		createdDate: new Date().toLocaleString()
-	},
-];
+export const initialState = {
+	tags: [],
+	tasks: [
+		{
+			title: "sample task title...",
+			description: "sample task description...",
+			group: "todo",
+			id: 1,
+			createdDate: new Date().toLocaleString(),
+		},
+	],
+};
 
 const tasksReducer = (state, action) => {
 	const { type, payload } = action;
 	switch (type) {
 		case "ADD_TASK_TO_GROUP":
 			console.log("ADD_TASK_TO_GROUP", payload);
-			return payload.tasks;
+			return { ...state, tasks: payload.tasks };
 		case "REMOVE_TASK":
 			console.log("REMOVE_TASK", payload);
-			return payload.tasks;
+			return { ...state, tasks: payload.tasks };
 		case "EDIT_TASK":
 			console.log("EDIT_TASK");
-			const index = state.findIndex(task=> task.id === payload.id)
-			const tempTasks = [...state];
-			tempTasks[index] = payload.editedTask
-			return tempTasks;
+			return { ...state, tasks: payload.tasks };
 		case "DUPLICATE_TASK":
 			console.log("DUPLICATE_TASK");
-			return payload.tasks
+			return { ...state, tasks: payload.tasks };
 		case "CHANGE_GROUP":
 			console.log("CHANGE_GROUP");
-			return state
+			return state;
 		default:
 			return state;
 	}
