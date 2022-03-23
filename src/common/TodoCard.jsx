751cf73bd5ca5d2248bcc33fcd4c51b22a7a36af
erfan shafiee <erfan74sh@gmail.com>
2022-03-23 17:47:07 +0430

    fix gap-y showing when there is no tags and no description in TodoCard component

--- src/common/TodoCard.jsx
+++ src/common/TodoCard.jsx
@@ -23,7 +23,7 @@ const TodoCard = ({ title, description, id, tags }) => {
 			className="bg-white rounded-md py-2 px-2.5 cursor-pointer"
 			onClick={(e) => handleClickOnCard(e)}
 		>
-			<article className="overflow-hidden flex flex-col gap-y-1">
+			<article className="flex flex-col gap-y-1">
 				<header>
 					<h3 className="font-medium flex justify-between gap-x-0.5 relative">
 						<span className="pr-6">{title}</span>
@@ -36,10 +36,12 @@ const TodoCard = ({ title, description, id, tags }) => {
 						</div>
 					</h3>
 				</header>
-				<section>{description}</section>
-				<section>
-					<Tags tags={tags} />
-				</section>
+				{description && <section>{description}</section>}
+				{tags.length > 0 && (
+					<section>
+						<Tags tags={tags} />
+					</section>
+				)}
 			</article>
 		</li>
 	);
