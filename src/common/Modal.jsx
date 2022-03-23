8a6bf757c2f919fb517a5121725ca7425c500253
erfan shafiee <erfan74sh@gmail.com>
2022-03-23 17:19:56 +0430

    add flex-grow to Tags container

--- src/common/Modal.jsx
+++ src/common/Modal.jsx
@@ -194,7 +194,7 @@ const Modal = () => {
 											checkedTags={checkedTags}
 										/>
 									</div>
-									<div className="w-1/2 sm:w-1/3 lg:w-1/4 flex items-center">
+									<div className="w-1/2 sm:w-1/3 lg:w-1/4 flex items-center flex-grow">
 										<Tags tags={taskToEdit.tags} />
 									</div>
 								</li>
