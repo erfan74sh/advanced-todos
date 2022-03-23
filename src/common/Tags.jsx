c14aca9b23c2143845d52ea6a9e4fd5ac35abaa5
erfan shafiee <erfan74sh@gmail.com>
2022-03-23 21:48:07 +0430

    add bg-color of each tag based on it's color

--- src/common/Tags.jsx
+++ src/common/Tags.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+// context
+import { useTasksContext } from "../providers/TasksProvider";
 
 const Tag = ({ children }) => {
+	const { tags } = useTasksContext();
+
+	const bgColor = tags.filter((tag) => tag.tagName === children)[0].color;
+
 	return (
-		<li className="px-1 py-0.5 bg-red-300 text-sm leading-none rounded cursor-default">
+		<li
+			className="px-1 py-0.5 text-sm leading-none rounded cursor-default"
+			style={{ backgroundColor: bgColor }}
+		>
 			<span>{children}</span>
 		</li>
 	);
