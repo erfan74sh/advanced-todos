d43230f58cd8e0362f6bb0a1872f99200d8685c9
erfan shafiee <erfan74sh@gmail.com>
2022-03-23 14:46:32 +0430

    create Tag component inside Tags component

--- src/common/Tags.jsx
+++ src/common/Tags.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
+const Tag = ({ children }) => {
+	return <li>{children}</li>;
+};
+
 const Tags = ({ currentTask }) => {
 	return (
 		<ul className="flex gap-x-2">
 			{currentTask.tags &&
 				currentTask.tags.map((tag, idx) => {
-					return <li key={idx}>{tag}</li>;
+					return <Tag key={idx}>{tag}</Tag>;
 				})}
 		</ul>
 	);
