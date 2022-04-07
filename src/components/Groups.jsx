110cc4f7fbe0788e821cc93e23f105031c5d1a7e
erfan shafiee <erfan74sh@gmail.com>
2022-04-07 21:15:31 +0430

    define draggedRef variable to share between Group components

--- src/components/Groups.jsx
+++ src/components/Groups.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useRef } from "react";
 // components
 import Group from "../common/Group";
 
 const Groups = () => {
+	const draggedRef = useRef();
+
 	return (
 		<div className="flex gap-x-10 ">
-			<Group groupName="todo" />
-			<Group groupName="doing" />
-			<Group groupName="completed" />
+			<Group groupName="todo" draggedRef={draggedRef} />
+			<Group groupName="doing" draggedRef={draggedRef} />
+			<Group groupName="completed" draggedRef={draggedRef} />
 		</div>
 	);
 };
