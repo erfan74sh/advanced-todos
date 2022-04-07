8be31f85f24daf0b6ac1279507af1dd3603d5641
erfan shafiee <erfan74sh@gmail.com>
2022-04-07 15:35:46 +0430

    initilize handleDrop and handleDragOver fn

--- src/common/Group.jsx
+++ src/common/Group.jsx
@@ -13,8 +13,21 @@ const Group = ({ groupName }) => {
 		setTasksInGroup(filteredTasks);
 	}, [tasks, groupName]);
 
+	const handleDrop = (e) => {
+		e.preventDefault();
+		const cardId = e.dataTransfer.getData("cardId");
+	};
+
+	const handleDragOver = (e) => {
+		e.preventDefault();
+	};
+
 	return (
-		<article className="w-72 bg-blue-50 rounded-xl p-4 flex flex-col gap-y-3 min-h-screen">
+		<article
+			className="w-72 bg-blue-50 rounded-xl p-4 flex flex-col gap-y-3 min-h-screen"
+			onDrop={handleDrop}
+			onDragOver={handleDragOver}
+		>
 			<header className="flex justify-between items-center">
 				<h2 className="font-medium capitalize">{groupName}</h2>
 				<span className="py-0.5 px-2 max-w-full rounded bg-blue-100 text-sm text-sky-900">
