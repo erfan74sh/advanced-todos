4cb9c316d46badf19757e02523f444b43c90af4c
erfan shafiee <erfan74sh@gmail.com>
2022-04-11 14:32:22 +0430

    define states for each group and pass tasksInGroup to Group component

--- src/components/Groups.jsx
+++ src/components/Groups.jsx
@@ -1,15 +1,45 @@
-import React, { useRef } from "react";
+import React, { useRef, useState, useEffect } from "react";
 // components
 import Group from "../common/Group";
+import { useTasksContext } from "../providers/TasksProvider";
 
 const Groups = () => {
 	const draggedRef = useRef();
 
+	const { tasks } = useTasksContext();
+
+	const [todoTasks, setTodoTasks] = useState([]);
+	const [doingTasks, setDoingTasks] = useState([]);
+	const [completedTasks, setCompletedTasks] = useState([]);
+
+	useEffect(() => {
+		const filteredTodoTasks = tasks.filter((task) => task.group === "todo");
+		setTodoTasks(filteredTodoTasks);
+		const filteredDoingTasks = tasks.filter((task) => task.group === "doing");
+		setDoingTasks(filteredDoingTasks);
+		const filteredCompletedTasks = tasks.filter(
+			(task) => task.group === "completed"
+		);
+		setCompletedTasks(filteredCompletedTasks);
+	}, [tasks]);
+
 	return (
 		<div className="flex gap-x-10 ">
-			<Group groupName="todo" draggedRef={draggedRef} />
-			<Group groupName="doing" draggedRef={draggedRef} />
-			<Group groupName="completed" draggedRef={draggedRef} />
+			<Group
+				groupName="todo"
+				draggedRef={draggedRef}
+				tasksInGroup={todoTasks}
+			/>
+			<Group
+				groupName="doing"
+				draggedRef={draggedRef}
+				tasksInGroup={doingTasks}
+			/>
+			<Group
+				groupName="completed"
+				draggedRef={draggedRef}
+				tasksInGroup={completedTasks}
+			/>
 		</div>
 	);
 };
