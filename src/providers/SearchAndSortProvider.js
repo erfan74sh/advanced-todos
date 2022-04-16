e2bff441448a64a14fde443792f43c9dbf6f51c5
erfan shafiee <erfan74sh@gmail.com>
2022-04-16 21:20:20 +0430

    create SearchAndSort provider

---
+++ src/providers/SearchAndSortProvider.js
@@ -0,0 +1,38 @@
+import React, { createContext } from "react";
+import { useContext, useReducer } from "react/cjs/react.production.min";
+import searchAndSortReducer, {
+	initialState,
+} from "../reducers/searchAndSortReducer";
+
+const searchAndSortContext = createContext(initialState);
+
+const SearchAndSortProvider = ({ children }) => {
+	const [state, dispatch] = useReducer(searchAndSortReducer, initialState);
+
+	const updateSearchValue = (value) => {
+		dispatch({ type: "UPDATE_SEARCH_VALUE", payload: { searchValue: value } });
+	};
+
+	const value = {
+		searchValue: state.searchValue,
+		sortValue: state.searchValue,
+		updateSearchValue,
+	};
+	return (
+		<searchAndSortContext.Provider value={value}>
+			{children}
+		</searchAndSortContext.Provider>
+	);
+};
+
+export default SearchAndSortProvider;
+
+export const useSearchAndSortContext = () => {
+	const context = useContext(searchAndSortContext);
+	if (context === "undefined") {
+		throw new Error(
+			"useSearchAndSortContext must used within searchAndSortContext"
+		);
+	}
+	return context;
+};
