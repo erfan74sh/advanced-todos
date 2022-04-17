c2bf7ee914ecabee6dafaabd3f596d255492a4a4
erfan shafiee <erfan74sh@gmail.com>
2022-04-17 15:21:56 +0430

    responsive design in Sort component

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -4,51 +4,61 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
 	faArrowDownShortWide,
 	faArrowDownWideShort,
-	faCaretDown
+	faCaretDown,
 } from "@fortawesome/free-solid-svg-icons";
 
 const Sort = () => {
 	const [showDropdown, setShowDropdown] = useState(false);
 
-  const [sortBy, setSortBy] = useState("")
+	const [sortBy, setSortBy] = useState("");
 
-  const handleChange = (e) => {
-    setSortBy(e.target.value)
-    setShowDropdown(false)
-  }
+	const handleChange = (e) => {
+		setSortBy(e.target.value);
+		setShowDropdown(false);
+	};
 
-	const [sortOrder, setSortOrder] = useState('descending')
+	const [sortOrder, setSortOrder] = useState("descending");
 
-	  const handleOrderChange = (e) => {
-			setSortOrder(e.target.value)
-		}
+	const handleOrderChange = (e) => {
+		setSortOrder(e.target.value);
+	};
 
 	return (
-		<div className="relative flex gap-x-3 text-zinc-600">
-		{sortBy !== "" && (
-				<div className="flex gap-x-1" onChange={(e)=> handleOrderChange(e)}>
-					<label className={`cursor-pointer px-1.5 py-2 leading-none hover:text-sky-900 rounded ${sortOrder === "ascending" && "bg-zinc-200"}`} title="ascending">
-							<span>
-								<FontAwesomeIcon icon={faArrowDownShortWide} />
-							</span>
-							<input
-								type="radio"
-								name="sortOrder"
-								value="ascending"
-								className="hidden"
-							/>
-						</label>
-					<label className={`cursor-pointer px-1.5 py-2 leading-none hover:text-sky-900 rounded ${sortOrder === "descending" && "bg-zinc-200"}`} title="descending">
-							<span>
-								<FontAwesomeIcon icon={faArrowDownWideShort} />
-							</span>
-							<input
-								type="radio"
-								name="sortOrder"
-								value="descending"
-								className="hidden"
-							/>
-						</label>
+		<div className="relative flex gap-x-3 text-zinc-600 self-end">
+			{sortBy !== "" && (
+				<div className="flex gap-x-1" onChange={(e) => handleOrderChange(e)}>
+					<label
+						className={`cursor-pointer px-1.5 py-2 leading-none hover:text-sky-900 rounded ${
+							sortOrder === "ascending" && "bg-zinc-200"
+						}`}
+						title="ascending"
+					>
+						<span>
+							<FontAwesomeIcon icon={faArrowDownShortWide} />
+						</span>
+						<input
+							type="radio"
+							name="sortOrder"
+							value="ascending"
+							className="hidden"
+						/>
+					</label>
+					<label
+						className={`cursor-pointer px-1.5 py-2 leading-none hover:text-sky-900 rounded ${
+							sortOrder === "descending" && "bg-zinc-200"
+						}`}
+						title="descending"
+					>
+						<span>
+							<FontAwesomeIcon icon={faArrowDownWideShort} />
+						</span>
+						<input
+							type="radio"
+							name="sortOrder"
+							value="descending"
+							className="hidden"
+						/>
+					</label>
 				</div>
 			)}
 			<div
@@ -57,14 +67,25 @@ const Sort = () => {
 			>
 				<div>
 					<span>Sort by </span>
-					{sortBy && <span className="text-sky-900 font-medium capitalize">{sortBy}</span>}
+					{sortBy && (
+						<span className="text-sky-900 font-medium capitalize">
+							{sortBy}
+						</span>
+					)}
 				</div>
 				<FontAwesomeIcon icon={faCaretDown} />
 			</div>
 			{showDropdown && (
-				<ul className="flex flex-col px-2 py-2.5 w-32 absolute right-0 rounded-md -bottom-1 transform translate-y-full bg-zinc-50 shadow-lg z-50" onChange={(e)=> handleChange(e)}>
+				<ul
+					className="flex flex-col px-2 py-2.5 w-32 absolute right-0 rounded-md -bottom-1 transform translate-y-full bg-zinc-50 shadow-lg z-50"
+					onChange={(e) => handleChange(e)}
+				>
 					<li>
-						<label className={`block cursor-pointer px-1.5 py-2 leading-none hover:bg-zinc-200 rounded ${sortBy === "name" && "text-sky-900"}`}>
+						<label
+							className={`block cursor-pointer px-1.5 py-2 leading-none hover:bg-zinc-200 rounded ${
+								sortBy === "name" && "text-sky-900"
+							}`}
+						>
 							<span>Name</span>
 							<input
 								type="radio"
@@ -75,7 +96,11 @@ const Sort = () => {
 						</label>
 					</li>
 					<li>
-						<label className={`block cursor-pointer px-1.5 py-2 leading-none hover:bg-zinc-200 rounded ${sortBy === "date" && "text-sky-900"}`}>
+						<label
+							className={`block cursor-pointer px-1.5 py-2 leading-none hover:bg-zinc-200 rounded ${
+								sortBy === "date" && "text-sky-900"
+							}`}
+						>
 							<span>Date</span>
 							<input
 								type="radio"
@@ -86,7 +111,11 @@ const Sort = () => {
 						</label>
 					</li>
 					<li>
-						<label className={`block cursor-pointer px-1.5 py-2 leading-none hover:bg-zinc-200 rounded ${sortBy === "priority" && "text-sky-900"}`}>
+						<label
+							className={`block cursor-pointer px-1.5 py-2 leading-none hover:bg-zinc-200 rounded ${
+								sortBy === "priority" && "text-sky-900"
+							}`}
+						>
 							<span>Priority</span>
 							<input
 								type="radio"
@@ -97,14 +126,17 @@ const Sort = () => {
 						</label>
 					</li>
 					<li>
-						<label className={`block cursor-pointer px-1.5 py-2 leading-none hover:bg-zinc-200 rounded ${sortBy === "" && "text-sky-900"}`}>
+						<label
+							className={`block cursor-pointer px-1.5 py-2 leading-none hover:bg-zinc-200 rounded ${
+								sortBy === "" && "text-sky-900"
+							}`}
+						>
 							<span>None</span>
 							<input type="radio" name="sortBy" value="" className="hidden" />
 						</label>
 					</li>
 				</ul>
 			)}
-			
 		</div>
 	);
 };
