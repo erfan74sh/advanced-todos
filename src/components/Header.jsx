0143428b007b5e2e0934b0c5f3796c39dec4a9a9
erfan shafiee <erfan74sh@gmail.com>
2022-04-17 15:21:21 +0430

    responsive design in Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 // components
+import Search from "./Search";
 import Sort from "./Sort";
 
 const Header = () => {
 	return (
-		<header className="py-8 px-1 flex items-center justify-between gap-x-5">
-			<h1 className="text-3xl font-medium max-w-full">Todo List...</h1>
+		<header className="sm:py-8 pb-4 px-1 flex items-center justify-between gap-x-5 flex-col sm:flex-row gap-y-2">
+			<Search />
 			<Sort />
 		</header>
 	);
