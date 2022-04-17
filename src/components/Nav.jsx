442ebf3682533024c028c4d4f42c4a9e0f15a9c5
erfan shafiee <erfan74sh@gmail.com>
2022-04-17 15:21:29 +0430

    responsive design in Nav component

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Profile from "./Profile";
-import Search from "./Search";
 
 const Nav = () => {
 	return (
-		<nav className="flex justify-between py-9">
-			<Search />
+		<nav className="flex justify-between py-9 items-center gap-y-10 sm:flex-row flex-col-reverse">
+			<h1 className="text-4xl max-w-full py-10 sm:py-0 text-sky-900 font-black">
+				Todo List
+			</h1>
 			<Profile />
 		</nav>
 	);
