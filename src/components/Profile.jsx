f9bf516de9ec873ccae5a71f1a14759f228fc418
erfan shafiee <erfan74sh@gmail.com>
2022-04-19 13:26:21 +0430

    add link address to socials acconts

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Link } from "react-router-dom";
 // assets
 import ProfilPic from "../assets/myPic.jpg";
 // icons
@@ -17,35 +16,41 @@ const Profile = () => {
 		<div className="w-full sm:w-auto flex items-center gap-x-3 text-sky-900">
 			<ul className="flex items-center gap-x-2 text-2xl mr-auto">
 				<li className="ring-2 rounded-md w-9 h-9 flex items-center justify-center">
-					<Link to="" className="w-full h-full">
+					<a href="https://github.com/erfan74sh" className="w-full h-full">
 						<FontAwesomeIcon icon={faGithubSquare} className="w-full h-full" />
-					</Link>
+					</a>
 				</li>
 				<li className="ring-2 rounded-md w-9 h-9 flex items-center justify-center">
-					<Link to="" className="w-full h-full">
+					<a
+						href="https://www.instagram.com/erfanshafiee/"
+						className="w-full h-full"
+					>
 						<FontAwesomeIcon
 							icon={faInstagramSquare}
 							className="w-full h-full"
 						/>
-					</Link>
+					</a>
 				</li>
 				<li className="ring-2 rounded-md w-9 h-9 flex items-center justify-center">
-					<Link to="" className="w-full h-full">
+					<a
+						href="https://www.linkedin.com/in/erfan-shafiee/"
+						className="w-full h-full"
+					>
 						<FontAwesomeIcon icon={faLinkedin} className="w-full h-full" />
-					</Link>
+					</a>
 				</li>
 				<li className="ring-2 rounded-md w-9 h-9 flex items-center justify-center">
-					<Link to="" className="w-full h-full">
+					<a href="https://twitter.com/erfan95sh" className="w-full h-full">
 						<FontAwesomeIcon icon={faTwitterSquare} className="w-full h-full" />
-					</Link>
+					</a>
 				</li>
 				<li className="ring-2 rounded-md w-9 h-9 flex items-center justify-center">
-					<Link to="" className="w-full h-full">
+					<a href="mailto:erfanshafiee.dev@gmail.com" className="w-full h-full">
 						<FontAwesomeIcon
 							icon={faSquareEnvelope}
 							className="w-full h-full"
 						/>
-					</Link>
+					</a>
 				</li>
 			</ul>
 			<div className="flex items-center gap-x-2 ring-2 rounded-md pl-2">
