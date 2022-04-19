5b9a489885b19bce2d5b606f8db04cc336ab66fb
erfan shafiee <erfan74sh@gmail.com>
2022-04-19 13:29:41 +0430

    add target blank and noreferrer rel to links

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -16,13 +16,20 @@ const Profile = () => {
 		<div className="w-full sm:w-auto flex items-center gap-x-3 text-sky-900">
 			<ul className="flex items-center gap-x-2 text-2xl mr-auto">
 				<li className="ring-2 rounded-md w-9 h-9 flex items-center justify-center">
-					<a href="https://github.com/erfan74sh" className="w-full h-full">
+					<a
+						href="https://github.com/erfan74sh"
+						target="_blank"
+						rel="noreferrer"
+						className="w-full h-full"
+					>
 						<FontAwesomeIcon icon={faGithubSquare} className="w-full h-full" />
 					</a>
 				</li>
 				<li className="ring-2 rounded-md w-9 h-9 flex items-center justify-center">
 					<a
 						href="https://www.instagram.com/erfanshafiee/"
+						target="_blank"
+						rel="noreferrer"
 						className="w-full h-full"
 					>
 						<FontAwesomeIcon
@@ -34,18 +41,30 @@ const Profile = () => {
 				<li className="ring-2 rounded-md w-9 h-9 flex items-center justify-center">
 					<a
 						href="https://www.linkedin.com/in/erfan-shafiee/"
+						target="_blank"
+						rel="noreferrer"
 						className="w-full h-full"
 					>
 						<FontAwesomeIcon icon={faLinkedin} className="w-full h-full" />
 					</a>
 				</li>
 				<li className="ring-2 rounded-md w-9 h-9 flex items-center justify-center">
-					<a href="https://twitter.com/erfan95sh" className="w-full h-full">
+					<a
+						href="https://twitter.com/erfan95sh"
+						target="_blank"
+						rel="noreferrer"
+						className="w-full h-full"
+					>
 						<FontAwesomeIcon icon={faTwitterSquare} className="w-full h-full" />
 					</a>
 				</li>
 				<li className="ring-2 rounded-md w-9 h-9 flex items-center justify-center">
-					<a href="mailto:erfanshafiee.dev@gmail.com" className="w-full h-full">
+					<a
+						href="mailto:erfanshafiee.dev@gmail.com"
+						target="_blank"
+						rel="noreferrer"
+						className="w-full h-full"
+					>
 						<FontAwesomeIcon
 							icon={faSquareEnvelope}
 							className="w-full h-full"
