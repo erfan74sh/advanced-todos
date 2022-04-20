c08ae14e01edc20cb44e05ca7d420daa68b29950
erfan shafiee <erfan74sh@gmail.com>
2022-04-20 12:33:39 +0430

    change search container bg- color

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -8,7 +8,7 @@ import { faMagnifyingGlass, faXmark } from "@fortawesome/free-solid-svg-icons";
 const Search = () => {
 	const { searchValue, updateSearchValue } = useSearchAndSortContext();
 	return (
-		<div className="w-full sm:w-auto max-w-screen-sm sm:max-w-md flex-shrink flex rounded-md flex-grow focus-within:ring-2 sm:bg-transparent hover:bg-gray-50 bg-gray-50  transition-colors duration-300">
+		<div className="w-full sm:w-auto max-w-screen-sm sm:max-w-md flex-shrink flex rounded-md flex-grow focus-within:ring-2 sm:bg-transparent hover:bg-zinc-100 bg-zinc-100  transition-colors duration-300">
 			<label
 				htmlFor="search"
 				className="flex items-center justify-center text-2xl text-sky-900 w-9 h-9"
